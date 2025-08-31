b92ce9799ad9c795c9aa66bc9209f37fb25a790c
ss3on <joehqh@gmail.com>
2025-08-31 22:58:14 +0800

    pdf parsing

--- scripts/pdf_md.py
+++ scripts/pdf_md.py
@@ -1,13 +1,21 @@
-def main():
-    import time
-    from docling.document_converter import DocumentConverter
+import time
+from docling.document_converter import DocumentConverter
 
-    from src.file_handling import file_location
+import bootstrap
+bootstrap.patch_sys_path()
+from src.file_handling import file_location
+
+
+import telegram_update
+
+notifier = telegram_update.TelegramNotifier(window_size=10)
+
+def main():
+
 
     data = file_location.FolderPathOfASME()
     markdown_path = data.asme_jmd / 'markdown'
     pdf_path = data.asme_jmd_pdf
-    pdf_path_list = pdf_path.rglob('*.pdf')
 
 
     converter = DocumentConverter().convert
@@ -15,8 +23,10 @@ def main():
     if not markdown_path.exists(): markdown_path.mkdir()
 
     export_times = []
-    for pdf in pdf_path_list:
+    session_process_time :float = 0.0
+    for pdf in pdf_path.rglob('*.pdf'):
         if pdf.stem + '.md' not in [p.name for p in markdown_path.glob('*.md')]:
+            print(f'docling processing: {pdf.name}')
             start_time = time.time()
             doc = converter(pdf)
             file_export_path = markdown_path / (pdf.stem + '.md')
@@ -26,8 +36,11 @@ def main():
                 f.write(md_content)
 
             elapsed = time.time() - start_time
+            session_process_time += elapsed
             export_times.append(elapsed)
-            print(f"Exported {pdf.stem + '.md'} in {elapsed:.2f}s")
+            processed_message = f"Exported {pdf.stem + '.md'} in {elapsed:.2f}s. Session Total: {session_process_time:.2f}s"
+            print(processed_message)
+            notifier.add_message(processed_message)
 
 
 if __name__ == "__main__":
